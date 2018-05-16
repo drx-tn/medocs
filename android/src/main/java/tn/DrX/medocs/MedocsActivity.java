84085eb7668acca1c639b4862bf45f6394a3f5fa
DrX@TUN <39054543+drxtun@users.noreply.github.com>
2018-05-16 19:56:55 +0100

    Début de travail sur Android (squelette Gradle).

---
+++ android/src/main/java/tn/DrX/medocs/MedocsActivity.java
@@ -0,0 +1,12 @@
+package tn.drx.medocs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Context;
+
+public class MedocsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.medocs);
+    }
+}
